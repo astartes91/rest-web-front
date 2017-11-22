1be98ae434bdcd0a3b2388247da63f428e45c828
Vladimir Nizamutdinov <v.nizamutdinov@softage.ru>
2017-11-22 18:35:46 +0700

    Changes

---
+++ src/main/java/org/bibliarij/aurus5assignment/aurus5assignment/CategoryController.java
@@ -0,0 +1,10 @@
+package org.bibliarij.aurus5assignment.aurus5assignment;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Vladimir Nizamutdinov
+ */
+@RestController
+public class CategoryController {
+}
