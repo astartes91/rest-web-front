8d65750d452e99fe1c57f8b0edc1bf98912a1ba3
Vladimir Nizamutdinov <v.nizamutdinov@softage.ru>
2017-11-22 18:06:33 +0700

    Initial commit

---
+++ src/main/java/org/bibliarij/aurus5assignment/aurus5assignment/MainApplication.java
@@ -0,0 +1,12 @@
+package org.bibliarij.aurus5assignment.aurus5assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
