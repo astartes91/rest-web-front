09ad87d27c027fa19b02c842d6cf6024744e4122
unknown <astartes91@gmail.com>
2017-12-02 22:43:04 +0700

    UI changes

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,37 @@
-$(document).ready(function () {
-    $.get("/categories", function (data) {
-        alert(data);
-    });
-});
+$(document).ready(
+    function () {
+        $("showCategoriesButton").click(
+            function () {
+                getCategories();
+            }
+        );
+
+        getCategories();
+    }
+);
+
+function getCategories() {
+
+    $.get(
+        "/categories",
+        function (data) {
+            var rows = $("#categoriesTable").children();
+            for (var i = 1; i < rows.length; i++){
+                rows[i].remove();
+            }
+
+            for (var i = 0; i < data.length; i++){
+
+                var element = data[i];
+                var name = element.name;
+                var description = element.description;
+
+                var row = $("<tr></tr>");
+                row.append($("<td>" + name + "</td>"));
+                row.append($("<td>" + description + "</td>"));
+
+                $("#categoriesTable tr:last").after(row);
+            }
+        }
+    );
+}
